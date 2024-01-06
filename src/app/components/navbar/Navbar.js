c8cd50a28d8055a19e0332fc81944301e06ec50e
contiguima <contiguima@gmail.com>
2024-01-06 19:20:37 -0300

    [FEAT] project structure and initial steps onto the navbar

---
+++ src/app/components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+import "./navbar.css"
+
+const Navbar = () => {
+    return(
+    <div className="navbar-container">
+        <ul className= "navbar-ul">
+            <li>PROJECTS</li>
+            <li>EXPERIENCE</li>
+            <li>EDUCATION</li>
+            <li>LET'S TALK</li>
+        </ul>
+    </div>)
+
+};
+
+export default Navbar;
