1b53b6320268b8fe4b45d2a846210efdd5040ecb
contiguima <contiguima@gmail.com>
2024-01-07 18:54:27 -0300

    [FEAT] Navbar finished, initial structure of home section

--- src/app/components/navbar/Navbar.js
+++ src/app/components/navbar/Navbar.js
@@ -1,15 +1,40 @@
-import "./navbar.css"
+"use client"
+import "./navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+
+    const[isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu =() =>{
+        setMenuOpen(!isMenuOpen);
+    }
+
+
     return(
-    <div className="navbar-container">
+    <nav className="navbar-container">
+        <button className="navbar-button" onClick={toggleMenu}><svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg></button>
+
+        <div className={isMenuOpen? "navbar-menu expanded" : "navbar-menu"}>
         <ul className= "navbar-ul">
-            <li>PROJECTS</li>
-            <li>EXPERIENCE</li>
-            <li>EDUCATION</li>
-            <li>LET'S TALK</li>
+            <li> <a>PROJECTS</a></li>
+            <li><a>EXPERIENCE</a></li>
+            <li><a>EDUCATION</a></li>
+            <li ><a className="contact-button">LET'S TALK</a></li>
         </ul>
-    </div>)
+        </div>
+    </nav>)
 
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,26 @@ import "./globals.css";
 
 export default function Home() {
   return (
-   <div className="home">
+   <div className="app">
    <Navbar/>
+    <section className="home">
+     <div className="left-bar">
+      <div className="picture"></div>
+      <h2>Constanza Guimaraez</h2>
+      <ul>
+        <li>Software development</li>
+        <li>Chemical Engineering</li>
+        <li>Biotechnology</li>
+      </ul>
+      <h3><i>Quick links</i></h3>
+      <div className="home-links">
+
+      </div>
+      </div> 
+     <div className="presentation">
+      <h1>Science, technology</h1>
+     </div>
+    </section>
    </div>
   )
 }
